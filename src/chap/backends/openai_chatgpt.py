a0a5c4f3035ffa4fa86877349f67846ceebf7325
Jeff Epler <jepler@gmail.com>
2023-03-15 15:59:33 -0500

    make chatgpt backend more resilient against exceptions

--- src/chap/backends/openai_chatgpt.py
+++ src/chap/backends/openai_chatgpt.py
@@ -48,39 +48,46 @@ class ChatGPT:
         session.session.extend([User(query), Assistant(result)])
         return result
 
-    async def aask(self, session, query, *, max_query_size=5):
+    async def aask(self, session, query, *, max_query_size=5, timeout=60):
         full_prompt = Session(session.session + [User(query)])
         del full_prompt.session[1:-max_query_size]
 
         new_content = []
-        async with httpx.AsyncClient() as client:
-            async with client.stream(
-                "POST",
-                "https://api.openai.com/v1/chat/completions",
-                headers={"authorization": f"Bearer {self.get_key()}"},
-                json={
-                    "model": "gpt-3.5-turbo",
-                    "stream": True,
-                    "messages": full_prompt.to_dict()[  # pylint: disable=no-member
-                        "session"
-                    ],  # pylint: disable=no-member
-                },
-            ) as response:
-                if response.status_code == 200:
-                    async for line in response.aiter_lines():
-                        if line.startswith("data:"):
-                            data = line.removeprefix("data:").strip()
-                            if data == "[DONE]":
-                                break
-                            j = json.loads(data)
-                            delta = j["choices"][0]["delta"]
-                            content = delta.get("content")
-                            if content:
-                                new_content.append(content)
-                                yield content
-                else:
-                    yield f"Failed with {response.status_code}"
-                    return
+        try:
+            async with httpx.AsyncClient(timeout=timeout) as client:
+                async with client.stream(
+                    "POST",
+                    "https://api.openai.com/v1/chat/completions",
+                    headers={"authorization": f"Bearer {self.get_key()}"},
+                    json={
+                        "model": "gpt-3.5-turbo",
+                        "stream": True,
+                        "messages": full_prompt.to_dict()[  # pylint: disable=no-member
+                            "session"
+                        ],  # pylint: disable=no-member
+                    },
+                ) as response:
+                    if response.status_code == 200:
+                        async for line in response.aiter_lines():
+                            if line.startswith("data:"):
+                                data = line.removeprefix("data:").strip()
+                                if data == "[DONE]":
+                                    break
+                                j = json.loads(data)
+                                delta = j["choices"][0]["delta"]
+                                content = delta.get("content")
+                                if content:
+                                    new_content.append(content)
+                                    yield content
+                    else:
+                        content = f"\nFailed with {response=!r}"
+                        new_content.append(content)
+                        yield content
+
+        except httpx.HTTPError as e:
+            content = f"\nException: {e!r}"
+            new_content.append(content)
+            yield content
 
         session.session.extend([User(query), Assistant("".join(new_content))])
 
