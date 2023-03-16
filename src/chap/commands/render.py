2e91d210a4a20442e9cb853635ee7c192640395d
Jeff Epler <jepler@gmail.com>
2023-03-16 11:38:23 -0500

    render: factor out to_markdown

--- src/chap/commands/render.py
+++ src/chap/commands/render.py
@@ -11,6 +11,21 @@ from ..core import last_session_path
 from ..session import Session
 
 
+def to_markdown(message):
+    role = message.role
+    if role == "user":
+        style = "bold"
+    elif role == "system":
+        style = "italic"
+    else:
+        style = "none"
+    m = Markdown("", style=style)
+    parser = MarkdownIt()
+    parser.options["html"] = False
+    m.parsed = parser.parse(message.content.strip())
+    return m
+
+
 @click.command
 @click.option("--session", "-s", type=click.Path(exists=True), default=None)
 @click.option("--last", is_flag=True)
@@ -29,18 +44,7 @@ def main(session, last):
         if not first:
             console.print()
         first = False
-        role = row.role
-        if role == "user":
-            style = "bold"
-        elif role == "system":
-            style = "italic"
-        else:
-            style = "none"
-        m = Markdown("", style=style)
-        parser = MarkdownIt()
-        parser.options["html"] = False
-        m.parsed = parser.parse(row.content)
-        console.print(m)
+        console.print(to_markdown(row))
 
 
 if __name__ == "__main__":
