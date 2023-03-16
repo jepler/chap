b1b622fbbe7262afa3e8bb46bf15e9806c848a84
Jeff Epler <jepler@gmail.com>
2023-03-16 11:40:09 -0500

    Add `chap grep`

---
+++ src/chap/commands/grep.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2023 Jeff Epler <jepler@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import pathlib
+import re
+from typing import Iterable, Optional, Tuple
+
+import click
+import rich
+
+from ..core import conversations_path as default_conversations_path
+from ..session import Message, Session
+from .render import to_markdown
+
+
+def list_files_matching_re(
+    pattern: str, ignorecase: bool = False, conversations_path: Optional[str] = None
+) -> Iterable[Tuple[pathlib.Path, Message]]:
+    rx = re.compile(pattern, re.I if ignorecase else 0)
+    for conversation in (conversations_path or default_conversations_path).glob(
+        "*.json"
+    ):
+        with open(conversation, "r", encoding="utf-8") as f:
+            session = Session.from_json(f.read())  # pylint: disable=no-member
+            for message in session.session:
+                if rx.search(message.content):
+                    yield conversation, message
+
+
+@click.command
+@click.option("--ignore-case", "-i", is_flag=True)
+@click.option("--fixed-strings", "--literal", "-F", is_flag=True)
+@click.argument("pattern", nargs=1, required=True)
+def main(ignore_case, fixed_strings, pattern):
+    console = rich.get_console()
+    if fixed_strings:
+        pattern = re.escape(pattern)
+
+    last_file = None
+    for f, m in list_files_matching_re(pattern, ignore_case):
+        if f != last_file:
+            if last_file:
+                print()
+            console.print(f"[bold]{f}[nobold]:")
+            last_file = f
+        else:
+            console.print("[dim]---[nodim]")
+        console.print(to_markdown(m))
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
